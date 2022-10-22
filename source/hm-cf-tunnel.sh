bad54444cbb02d57b4312d5a960749de2fae6cc7
Oskar Lorenz <oskar.lorenz@th-wildau.de>
2022-10-22 14:05:27 +0200

    Add `cloudflared` service status to addon info

--- source/hm-cf-tunnel.sh
+++ source/hm-cf-tunnel.sh
@@ -57,6 +57,10 @@ install)
 info)
     ADDON_VERSION=$(cat $ADDON_PATH/VERSION)
 
+    # Check the current service status.
+    CFD_SERVICE_STATUS=$($ADDON_PATH/cfd-service.sh status)
+    CFD_SERVICE_RUNNING=$?
+
     # Parse cloudflared version without build time.
     CFD_VERSION=$($CFD_PATH --version | sed 's: (.*::g')
 
@@ -64,8 +68,14 @@ info)
     echo "Version: $ADDON_VERSION"
 
     echo "Info: <b>$ADDON_DESCRIPTION</b><br>"
-    echo "Info: Copyright (c) 2022 Oskar Lorenz"
-    echo "Info: ($CFD_VERSION)"
+    echo "Info: <i>$CFD_VERSION</i><br>"
+
+    if [ $CFD_SERVICE_RUNNING -eq 0 ]; then
+        echo "Info: <p style='color: green'>Status: $CFD_SERVICE_STATUS</p>"
+    else
+        echo "Info: <p style='color: red'>Status: $CFD_SERVICE_STATUS</p>"
+        echo "Info: Try to restart the service or create a new tunnel."
+    fi
 
     echo "Update: $ADDON_WWW_URL/update.cgi"
     echo "Operations: uninstall restart"
