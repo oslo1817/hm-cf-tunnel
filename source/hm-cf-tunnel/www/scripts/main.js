75d30d692cb55561d1207fff2a74d55e7210d9fd
Oskar Lorenz <oskar.lorenz@th-wildau.de>
2022-10-28 17:44:49 +0200

    Update configuration website
    - Remove _Command Output_ card/section.
    - Add success & failure message overlays.

--- source/hm-cf-tunnel/www/scripts/main.js
+++ source/hm-cf-tunnel/www/scripts/main.js
@@ -1,14 +1,39 @@
 const $ = document.getElementById.bind(document);
 
 async function configure() {
+  disableConfigureForm();
+
   const token = $("configure-token").value;
 
   // Execute the configure command with token.
   const url = `api/configure.cgi?token=${token}`;
   const response = await fetch(url);
 
-  // Show command result in the command output.
-  $("command-output").value = await response.text();
+  if (response.ok) {
+    showConfigureOverlay("success");
+  } else {
+    showConfigureOverlay("failure");
+  }
+}
+
+function disableConfigureForm(disabled = true) {
+  $("configure-form").disabled = disabled;
+  $("configure-submit").disabled = disabled;
+}
+
+function showConfigureOverlay(type, visible = true, timeout = 5000) {
+  if (visible) {
+    disableConfigureForm();
+
+    $(`configure-overlay-${type}`).style.visibility = "visible";
+
+    // Hide the overlay again after the configured timeout.
+    setTimeout(() => showConfigureOverlay(type, false), timeout);
+  } else {
+    $(`configure-overlay-${type}`).style.visibility = "hidden";
+
+    disableConfigureForm(false);
+  }
 }
 
 $("configure-form").addEventListener("submit", configure);
